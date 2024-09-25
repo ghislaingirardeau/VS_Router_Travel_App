51ee159664894601245080aa9d07756eebc27f00
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-09-25 10:37:53 +0200

    set class

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="nav">
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink
-      v-for="destination in destinationsSlug"
-      :key="destination.id"
-      :to="`/destination/${destination.id}`"
-      >{{ destination.name }}
-    </RouterLink>
     <RouterLink to="brazil">Brazil</RouterLink>
     <RouterLink to="panama">Panama</RouterLink>
-    <RouterLink to="hawaii">Hawaii</RouterLink>
   </div>
 </template>
 
-<script setup>
-import { computed } from 'vue'
-import sourceData from '@/assets/data.json'
-
-const destinationsSlug = computed(() => {
-  return sourceData.destinations.map(({ id, slug, name }) => {
-    return { id, slug, name }
-  })
-})
-</script>
+<script setup></script>
 
 <style lang="css" scoped>
 #nav .custom-link-active-class {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Brazil from '../views/Brazil.vue'
+import Panama from '../views/Panama.vue'
 
 const routes = [
   {
@@ -7,33 +9,20 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  // lazy loading routes with import component
-  // in dev tool network, we will see that js for this page are loaded only when we navigate to it
-  // npm run build will create js file for each lazy load component
   {
     path: '/brazil',
     name: 'brazil',
-    // pour renomer le chunk si besoin (pas besoin dans vite)
-    component: () => import(/* webpackChunkName: "brazilChunk" */ '@/views/Brazil.vue')
+    component: Brazil
   },
   {
     path: '/panama',
     name: 'panama',
-    component: () => import('../views/Panama.vue')
-  },
-  {
-    path: '/hawaii',
-    name: 'hawaii',
-    component: () => import('../views/Hawaii.vue')
-  },
-  {
-    path: '/destination/:id',
-    component: () => import('../views/DestinationShow.vue')
+    component: Panama
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
   // to custom the css and name of the active link class (in TheNavigation component)
   linkActiveClass: 'custom-link-active-class'
